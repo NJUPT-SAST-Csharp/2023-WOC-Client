90f8fa5f05e60c26dffe0104a9e405f48df01683
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2023-12-24 00:55:36 +0800

    feat(UI): 通过dwm提供的api实现Mica Backdrop（网上抄的

--- SastWiki.WPF/MainWindow.xaml.cs
+++ SastWiki.WPF/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static SastWiki.WPF.Utils.SystemBackdrop.PInvoke.ParameterTypes;
+using static SastWiki.WPF.Utils.SystemBackdrop.PInvoke.Methods;
 
 namespace SastWiki.WPF
 {
@@ -19,6 +16,26 @@ namespace SastWiki.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr mainWindowPtr = new WindowInteropHelper(this).Handle;
+            HwndSource mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
+            mainWindowSrc.CompositionTarget.BackgroundColor = Color.FromArgb(0, 0, 0, 0);
+
+            MARGINS margins = new MARGINS();
+            margins.cxLeftWidth = -1;
+            margins.cxRightWidth = -1;
+            margins.cyTopHeight = -1;
+            margins.cyBottomHeight = -1;
+
+            ExtendFrame(mainWindowSrc.Handle, margins);
+            SetWindowAttribute(
+                new WindowInteropHelper(this).Handle,
+                DWMWINDOWATTRIBUTE.DWMWA_SYSTEMBACKDROP_TYPE,
+                2
+            );
         }
     }
 }
