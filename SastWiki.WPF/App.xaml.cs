2671bd56fd4705f46d14c9034e8f6c869e0c742d
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-01-25 23:31:59 +0800

    feat: 建立DI容器

--- SastWiki.WPF/App.xaml.cs
+++ SastWiki.WPF/App.xaml.cs
@@ -9,6 +9,38 @@ namespace SastWiki.WPF
     /// </summary>
     public partial class App : Application
     {
+        private IHost Host { get; }
+
+        public static T GetService<T>()
+            where T : class
+        {
+            if ((App.Current as App)!.Host.Services.GetService(typeof(T)) is not T service)
+            {
+                throw new ArgumentException(
+                    $"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs."
+                );
+            }
+
+            return service;
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            Host = Microsoft.Extensions.Hosting.Host
+                .CreateDefaultBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureServices(
+                    (context, services) => {
+                        // Register Services
+
+                        // Register ViewModels
+                    }
+                )
+                .Build();
+        }
+
         public void ConfigureServices(IServiceCollection services) { }
     }
 }
