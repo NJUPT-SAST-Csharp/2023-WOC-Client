9377e90bb13f89e76d438e0f7b27a1b1e56686a9
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-01-30 21:36:46 +0800

    doc: 给Navigation部分写文档😋 (#21)

--- SastWiki.WPF/Contracts/INavigationService.cs
+++ SastWiki.WPF/Contracts/INavigationService.cs
@@ -9,8 +9,20 @@ namespace SastWiki.WPF.Contracts
 {
     public interface INavigationService
     {
+        /// <summary>
+        /// 导航到指定页面并传递指定参数。需要目标页面实现<seealso cref="INavigationAware"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="page">要跳转到的页面实例，你可以从DI容器里面获取一个</param>
+        /// <param name="parameter">你要传递给要跳转到的页面的参数，在目标页面的<see cref="INavigationAware.OnNavigatedTo{T}(T)"/>中进一步处理</param>
+        /// <returns>是否跳转成功</returns>
         public Task<bool> NavigateTo<T>(Page page, T parameter);
 
+        /// <summary>
+        /// 导航到指定页面，不需要目标页面实现<seealso cref="INavigationAware"/>，因而也不能传递参数。
+        /// </summary>
+        /// <param name="page">要跳转到的页面实例，你可以从DI容器里面获取一个</param>
+        /// <returns>是否跳转成功</returns>
         public Task<bool> NavigateTo(Page page);
 
         public Task<bool> NavigateBackward();
