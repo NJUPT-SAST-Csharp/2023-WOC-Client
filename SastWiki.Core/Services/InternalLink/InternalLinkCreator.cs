ee1c04b5d45dcdfa60e30f137d128de21d2f1831
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-02-02 01:36:15 +0800

    feat: 初步实现内部链接支持 (#23)

---
+++ SastWiki.Core/Services/InternalLink/InternalLinkCreator.cs
@@ -0,0 +1,35 @@
+﻿using SastWiki.Core.Contracts.InternalLink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SastWiki.Core.Services.InternalLink
+{
+    public class InternalLinkCreator(IInternalLinkValidator validator, IInternalLinkService service)
+        : IInternalLinkCreator
+    {
+        public const string _schemeName = "wiki";
+        public const string _hostName = "sast-wiki";
+
+        public Uri Create(string path, Dictionary<string, string> query)
+        // NameValueCollection的性能似乎不如Dictionary<string,string>？
+        {
+            if (!service.ContainsPath(path))
+                throw new ArgumentException("path is not registered");
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{validator.SchemeName}://{validator.HostName}{path}");
+            if (query.Count > 0)
+            {
+                sb.Append("?");
+                foreach (KeyValuePair<string, string> kvp in query)
+                {
+                    sb.Append($"{kvp.Key}={kvp.Value}&");
+                }
+                sb.Remove(sb.Length - 1, 1);
+            }
+            return new Uri(sb.ToString());
+        }
+    }
+}
