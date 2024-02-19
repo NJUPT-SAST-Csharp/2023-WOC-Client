b36c88b9cb26b89dcb435cc95a6ffb5a2c1d3d85
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-02-19 18:42:35 +0800

    push forward

---
+++ SastWiki.Core/Models/Dto/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SastWiki.Core.Models.Dto
+{
+    public class UserDto
+    {
+        public string? Username { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? Token { get; set; }
+        public string? Role { get; set; }
+    }
+}
