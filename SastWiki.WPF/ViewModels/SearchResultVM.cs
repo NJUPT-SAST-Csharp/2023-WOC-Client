1366d1fa77a1aadcc64839dbb2e1aea5ad440087
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-02-22 22:07:11 +0800

    feat: 开始搓搜索页面

--- SastWiki.WPF/ViewModels/SearchResultVM.cs
+++ SastWiki.WPF/ViewModels/SearchResultVM.cs
@@ -1,22 +1,22 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-using SastWiki.WPF.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SastWiki.WPF.Contracts;
 
 namespace SastWiki.WPF.ViewModels
 {
     public partial class SearchResultVM : ObservableObject, INavigationAware
     {
         [ObservableProperty]
-        private string inquiryText;
+        private string _searchText;
 
         public SearchResultVM()
         {
-            InquiryText = "123";
+            SearchText = "";
         }
 
         Task<bool> INavigationAware.OnNavigatedFrom()
@@ -26,10 +26,9 @@ namespace SastWiki.WPF.ViewModels
 
         Task<bool> INavigationAware.OnNavigatedTo<T>(T parameters)
         {
-            // MessageBox.Show("OnNavigatedTo");
             if (parameters is string s)
             {
-                InquiryText = s;
+                SearchText = s;
             }
             return Task.FromResult(true);
         }
