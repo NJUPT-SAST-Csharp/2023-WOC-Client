13752670b8f382456a8c0d6ac3a6f4591e437cc7
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-02-23 02:36:44 +0800

    feat: TagViewer控件

---
+++ SastWiki.WPF/UserControl/TagViewer.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json.Linq;
+using YamlDotNet.Core.Tokens;
+
+namespace SastWiki.WPF.UserControl
+{
+    /// <summary>
+    /// TagViewer.xaml 的交互逻辑
+    /// </summary>
+    [ObservableObject]
+    public partial class TagViewer : System.Windows.Controls.UserControl
+    {
+        public TagViewer()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        [ObservableProperty]
+        public List<TagViewerTagItem> tagViewerTags;
+
+        public List<string> Tags
+        {
+            get { return (List<string>)GetValue(TagsProperty); }
+            set
+            {
+                SetValue(TagsProperty, value);
+                SetProperty(
+                    ref tagViewerTags,
+                    value
+                        .Select(x => new TagViewerTagItem() { Name = x, ClickCommand = Click })
+                        .ToList(),
+                    "TagViewerTags"
+                );
+                OnPropertyChanged("TagViewerTags");
+            }
+        }
+
+        public static readonly DependencyProperty TagsProperty = DependencyProperty.Register(
+            "Tags",
+            typeof(List<string>),
+            typeof(TagViewer),
+            new PropertyMetadata(new List<string>())
+        );
+
+        public ICommand Click
+        {
+            get { return (ICommand)GetValue(ClickProperty); }
+            set { SetValue(ClickProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Click.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ClickProperty = DependencyProperty.Register(
+            "Click",
+            typeof(ICommand),
+            typeof(TagViewer)
+        );
+    }
+
+    public record TagViewerTagItem()
+    {
+        public required string Name { get; init; }
+        public ICommand? ClickCommand { get; init; }
+    }
+}
--- SastWiki.WPF/ViewModels/SearchResultVM.cs
+++ SastWiki.WPF/ViewModels/SearchResultVM.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SastWiki.Core.Models.Dto;
 using SastWiki.WPF.Contracts;
 
 namespace SastWiki.WPF.ViewModels
@@ -14,9 +17,38 @@ namespace SastWiki.WPF.ViewModels
         [ObservableProperty]
         private string _searchText;
 
+        [ObservableProperty]
+        private List<EntryDto> _searchResult =
+        [
+            new()
+            {
+                Id = 1,
+                Title = "Test",
+                Content = "Testsafdafaf",
+                CategoryName = "SAST",
+                TagNames = ["2024", "Feburary"]
+            },
+            new()
+            {
+                Id = 2,
+                Title = "Sast Wiki Test",
+                Content = "Testsafdafaf",
+                CategoryName = "NJUPT",
+                TagNames = ["2023", "January"]
+            }
+        ];
+
         public SearchResultVM()
         {
             SearchText = "";
+        }
+
+        public ICommand TagClickCommand => new RelayCommand<string>(TagClick);
+
+        private void TagClick(string? tag)
+        {
+            MessageBox.Show($"Tag Clicked! {tag}");
+            // NavigationService.NavigateTo("SearchResult", tag);
         }
 
         Task<bool> INavigationAware.OnNavigatedFrom()
