ba6c679c13bb092cf73fe092e472c716128b570b
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-02-25 10:57:42 +0800

    漏了一处try-catch，并且加入了编辑后提交词条使按钮不可用的功能

--- SastWiki.WPF/ViewModels/HomePageVM.cs
+++ SastWiki.WPF/ViewModels/HomePageVM.cs
@@ -48,11 +48,22 @@ namespace SastWiki.WPF.ViewModels
         public ICommand LatestChangedEntryCommand =>
             new RelayCommand(async () =>
             {
-                var id = (await entryProvider.GetEntryMetadataList())
-                    .Select(x => x.Id ?? 0)
-                    .Where(x => x != 0)
-                    .Max();
-                await navigationService.NavigateTo<int>(App.GetService<EntryViewPage>(), id);
+                try
+                {
+                    var id = (await entryProvider.GetEntryMetadataList())
+                        .Select(x => x.Id ?? 0)
+                        .Where(x => x != 0)
+                        .Max();
+                    await navigationService.NavigateTo<int>(App.GetService<EntryViewPage>(), id);
+                }
+                catch (ApiException e)
+                {
+                    MessageBox.Show(e.Message, e.Content);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
             });
 
         Task<bool> INavigationAware.OnNavigatedFrom()
